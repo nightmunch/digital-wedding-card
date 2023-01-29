61fc7c4f333f2d086489f921848767a8efca6f64
Shahrin Amin <shahrinamin.sharifudin@tm.com.my>
2023-01-29 19:26:50 +0800

    nextauth secret

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -8,10 +8,10 @@ import { z } from "zod";
 export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
-  // NEXTAUTH_SECRET:
-  //   process.env.NODE_ENV === "production"
-  //     ? z.string().min(1)
-  //     : z.string().min(1).optional(),
+  NEXTAUTH_SECRET:
+    process.env.NODE_ENV === "production"
+      ? z.string().min(1)
+      : z.string().min(1).optional(),
   // NEXTAUTH_URL: z.preprocess(
   //   // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
   //   // Since NextAuth.js automatically uses the VERCEL_URL if present.
@@ -29,7 +29,7 @@ export const serverSchema = z.object({
 export const serverEnv = {
   DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
-  // NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   // NEXTAUTH_URL: process.env.NEXTAUTH_URL,
 };
 

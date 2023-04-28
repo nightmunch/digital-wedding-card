313cee3b60d1997aa8fdbabc8ce24ef4c189552c
Shahrin Amin <shahrinamin.sharifudin@tm.com.my>
2023-04-28 19:49:20 +0800

    use image

--- src/pages/api/aimi.tsx
+++ src/pages/api/aimi.tsx
@@ -1,41 +1,32 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import { ImageResponse } from "@vercel/og";
 
 export const config = {
   runtime: "edge",
 };
 
-// eslint-disable-next-line import/no-anonymous-default-export, @typescript-eslint/require-await
-export default async function () {
+const image = fetch(new URL("./aimi-metadata.png", import.meta.url)).then(
+  (res) => res.arrayBuffer()
+);
+
+export default async function handler() {
+  const imageData = (await image) as unknown as string;
   // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
   return new ImageResponse(
     (
-      // Modified based on https://tailwindui.com/components/marketing/sections/cta-sections
       <div
         style={{
-          height: "100%",
-          width: "100%",
           display: "flex",
+          background: "#f6f6f6",
+          width: "100%",
+          height: "100%",
           flexDirection: "column",
-          alignItems: "center",
           justifyContent: "center",
-          backgroundColor: "white",
+          alignItems: "center",
         }}
-        tw="bg-gray-50"
       >
-        <div tw="flex">
-          <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8 justify-center">
-            <h2 tw="flex flex-col font-bold tracking-tight text-gray-900 text-center justify-center items-center gap-10">
-              <span tw="text-3xl sm:text-4xl">Anda Dijemput Hadir</span>
-              <span tw="text-5xl sm:text-6xl text-[#5f7534]">
-                Aimi 💚 Shahrin
-              </span>
-              <span tw="text-5xl sm:text-6xl text-[#5f7534]">
-                Danial 💚 Amalin
-              </span>
-              <span tw="text-3xl sm:text-4xl text-gray-600">17 Jun 2023</span>
-            </h2>
-          </div>
-        </div>
+        <img width="600" height="600" src={imageData} />
       </div>
     ),
     {
